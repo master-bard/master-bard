f1793193a3f21aac1acba63ffeaf221587437add
Andrés Felipe Saldarriaga <xiopipe@gmail.com>
2023-07-02 13:17:23 -0500

    feat: first config

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+import Backend from 'i18next-http-backend';
+
+i18next.use(Backend).init({
+	interpolation: { escapeValue: false },
+	resources: {
+		es: {
+			translation: {
+				hello: '¡Hola mundo!',
+			},
+		},
+		en: {
+			translation: {
+				hello: 'hello world',
+			},
+		},
+	},
+	lng: 'es',
+	fallbackLng: 'en',
+	debug: true,
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
 	<React.StrictMode>
-		<App />
+		<I18nextProvider i18n={i18next}>
+			<App />
+		</I18nextProvider>
 	</React.StrictMode>,
 );
--- apps/ui/src/pages/Home.tsx
+++ apps/ui/src/pages/Home.tsx
@@ -7,13 +7,16 @@ import {
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { useTranslation } from 'react-i18next';
 
 const Home: React.FC = () => {
+	const [t] = useTranslation();
 	return (
 		<IonPage>
 			<IonHeader>
 				<IonToolbar>
 					<IonTitle>Blank</IonTitle>
+					<IonTitle>{t('hello')}</IonTitle>
 				</IonToolbar>
 			</IonHeader>
 			<IonContent fullscreen>
