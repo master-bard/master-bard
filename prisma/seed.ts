52d28033a7543d0b50f1ab3e93c9e06f2206c5d2
DonaldMurillo <donald.murillo07@gmail.com>
2023-07-08 19:49:31 -0400

    feat: added prisma schema migration and seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+// TODO: GET DATA FROM .env
+async function main() {
+	// create two dummy articles
+	const sysAdmin = await prisma.user.upsert({
+		where: { username: 'SUPER_ADMIN' },
+		update: {},
+		create: {
+			username: 'SUPER_ADMIN',
+			email: 'your@email.com',
+			password: 'your hashed password',
+			role: 'SYS_ADMIN',
+		},
+	});
+
+	const user = await prisma.user.upsert({
+		where: { username: 'normal_user' },
+		update: {},
+		create: {
+			username: 'normal_user',
+			email: 'your2@email.com',
+			password: 'your hashed password ftw',
+		},
+	});
+
+	console.log({ sysAdmin, user });
+}
+
+// execute the main function
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		// close Prisma Client at the end
+		await prisma.$disconnect();
+	});
